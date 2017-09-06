6433a3b066fefd409fffe728c2c3d8c64493ca38
Viswasai-M <viswasai.maddula@gmail.com>
2017-09-06 11:09:05 -0700

    Improved Feyman-writing-prompt

--- exercises.js
+++ exercises.js
@@ -101,7 +101,7 @@ fizzBuzz(3);
 
 function isPrime(num) {
 if (num>1){
-    for( var i =2; i<num; ){
+    for( var i =2; i<num; i++){
         if(num%i === 0){
             return false;
             }
